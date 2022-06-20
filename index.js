6268500857c6cf0906635d9e8ee29b11cf9de4dd
Danilo <dandam3006@gmail.com>
2022-06-20 16:34:27 -0300

    Verification and user page

--- index.js
+++ index.js
@@ -1,38 +1,76 @@
 import express from "express";
 import cors from "cors";
 
-const server=express();
+const server = express();
 server.use(cors());
 server.use(express.json());
 
-const users=[];
-const tweets=[];
+const users = [];
+const tweets = [];
 
-server.post("/sign-up", (req, res)=> {
-    users.push(req.body);
-    res.send("OK");
-});
-
-server.post("/tweets", (req, res)=> {
-    tweets.push(req.body);
-    res.send("OK");
-});
-server.get("/tweets", (req, res)=> {
-    const last10Tweets=tweets.slice(-10);
-    last10Tweets.reverse();
-    if (users.length===0){
-        res.send([]);
-        return
-    }
-    for (let i=0; i<last10Tweets.length; i++){
-        for (let j=0; j<users.length; j++){
-            if (last10Tweets[i].username===users[j].username){
-                last10Tweets[i].avatar=users[j].avatar;
-                break;
-            }
+function userPicture(user) {
+    for (let j = 0; j < users.length; j++) {
+        if (user === users[j].username) {
+            return users[j].avatar;
         }
     }
+}
+
+server.post("/sign-up", (req, res) => {
+    if (!req.body.avatar || !req.body.username) {
+        res.status(400)
+        res.send("Todos os campos são obrigatórios!");
+        return;
+    }
+    users.push(req.body);
+    res.status(201)
+    res.send("OK");
+});
+
+server.post("/tweets", (req, res) => {
+    if (!req.body.tweet || !req.body.username || !users.some(el => el.username === req.body.username)) {
+        res.status(400);
+        if (!users.some(el => el.username === req.body.username)) {
+            res.send("Usuario inexistente");
+        } else {
+            res.send("Todos os campos são obrigatórios!");
+        }
+        return;
+    }
+    req.body.avatar=userPicture(req.body.username);
+    tweets.push(req.body);
+    res.status(201);
+    res.send("OK");
+});
+server.get("/tweets", (req, res) => {
+    const last10Tweets = tweets.slice(-10);
+    last10Tweets.reverse();
     res.send(last10Tweets);
+});
+
+server.get("/tweets/:teste", (req, res) => {
+
+    const user = req.params.teste;
+    let arrUserTweets = [];
+
+    if (!users.some(el => el.username === user)){
+        res.status(400);
+        res.send("Usuario inexistente");
+        return;
+    }
+
+
+    function filterUserTweets(el) {
+        if (el.username===user) {
+            return true;
+        }
+        return false;
+    }
+
+    arrUserTweets = tweets.filter(filterUserTweets);
+    arrUserTweets.reverse();
+
+    res.send([arrUserTweets]);
 });
 
 server.listen(5000);
